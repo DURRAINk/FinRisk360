0dd17d8afee8e254c876ebc5fea754af119202d9
durrain2003@hotmail.com <durrain2003@hotmail.com>
2026-09-23 13:46:07 +0000

    data assets: getting ready for azure ml

--- databricks/scripts/03_export_features.py
+++ databricks/scripts/03_export_features.py
@@ -1,10 +1,6 @@
-#upload the silver data to adls
-data = spark.read.table('finrisk360.default.slv_bank_transactions')
-data.write.mode('overwrite').option('mergeSchema', 'true').save('abfss://project-dataset@finrisk3605359934826.dfs.core.windows.net/.bank_transactions_processed')
-
 #uploading the gold data to adls
-url = 'abfss://project-dataset@finrisk3605359934826.dfs.core.windows.net/final_datasets'
+gld_url = 'abfss://azureml-blobstore-158438c9-9d7b-46f9-8cdd-abe002d5b885@finrisk3605359934826.dfs.core.windows.net/final-datasets'
 datasets = ['train','val', 'test']
 for dataset in datasets:
     data = spark.read.table(f"finrisk360.gold_datasets.{dataset}")
-    data.write.mode('overwrite').option('mergeSchema', 'true').save(f'{url}/{dataset}')
+    data.write.format('csv').mode('overwrite').option('mergeSchema', 'true').save(f'{gld_url}/{dataset}')
--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,29 +1,5 @@
-from pyspark.ml import Transformer
-from pyspark.ml.feature import StringIndexer, OneHotEncoder
 from pyspark.sql import functions as F
 
-
-class EncodingPipeline(Transformer):
-    def __init__(self, one_hot=None, freq=None):
-        super().__init__()
-        self.one_hot = one_hot or []
-        self.freq = freq or []
-
-    def _transform(self, df):
-        for c in self.one_hot:
-            idx = f"{c}_index"
-            vec = f"{c}_encoded"
-            df = StringIndexer(inputCol=c, outputCol=idx, handleInvalid="keep").fit(df).transform(df)
-            df = OneHotEncoder(inputCols=[idx], outputCols=[vec], dropLast=False).fit(df).transform(df).drop(idx)
-            df = df.drop(c)
-
-        for c in self.freq:
-            m = df.groupBy(c).count().withColumnRenamed("count", f"{c}_freq")
-            df = df.join(m, c, "left")
-            df = df.drop(c)
-
-        return df
-    
 
 def transaction_minute_extract(df, ts_col="transaction_datetime"):
     if ts_col not in df.columns:
